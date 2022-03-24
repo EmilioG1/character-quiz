ddd61f98c9a05f716cce79c375ef21c1d4dada62
Emilio Gonzales <emiliog1184@gmail.com>
2022-03-24 16:33:15 -0700

    Add inital forms with html and some js
    co-authored-by: Jake Edgar <jakeedgar1012@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("sunnyform").submit(function(event) {
+    event.preventDefault();
+    const seasons = $("select#season").val();
+    const animals = $("select#animal").val();
+    const jobs = $("select#job").val();
+
+    if ()
+
+
+  });
+});
